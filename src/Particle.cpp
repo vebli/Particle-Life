f5b12c36f81880b1a64df634bae59045b5d1fd0d
vebly <jh.haruna@protonmail.com>
2024-01-28 13:46:45 +0100

    :Changed warping logic to stop structures from scattering when warping.

--- src/Particle.cpp
+++ src/Particle.cpp
@@ -43,44 +43,23 @@ void Particle::update(){
     float windowY = window.getSize().y;
 
     // std::cout << velocity.x << "," << velocity.y << std::endl;
-    if(newPosition.x >= 0 && newPosition.y >= 0 && newPosition.x <= windowX && newPosition.y <= windowY){
-        sprite.setPosition(newPosition);
-        position = newPosition;
-    }
     
-    else{ // wrap around window 
-        // std::cout << "out of bound" << std::endl;
-        sf::Vector2f trajectory(newPosition.x - position.x, newPosition.y - position.y);
-        float* scalars = new float[4];
-        float float_min = std::numeric_limits<float>::min();
-        //scalars to calculate window border position
-        scalars[0] = (trajectory.x != 0) ? -position.x / trajectory.x : float_min;
-        scalars[1] = (trajectory.y != 0) ? -position.y / trajectory.y : float_min; 
-        scalars[2] = (trajectory.x != 0) ? (windowX - position.x) / trajectory.x : float_min; 
-        scalars[3] = (trajectory.y != 0) ? (windowY - position.y) / trajectory.y : float_min;
-
-        float smallestScalar = scalars[0];
-        float secondSmallestScalar = std::numeric_limits<float>::min();
-        for(int i = 0; i < 4; i++){
-            if(smallestScalar > scalars[i] ){
-                smallestScalar = scalars[i];
-            }
-        }
-        for(int i = 0; i < 4; i++){
-            if(scalars[i] > smallestScalar && scalars[i] < secondSmallestScalar){
-                secondSmallestScalar = scalars[i];
-            }
-        }
-        
-        sf::Vector2f newPosition(position.x + trajectory.x * secondSmallestScalar, position.y + trajectory.y * secondSmallestScalar);
-        newPosition.x += trajectory.x * delta_t;
-        newPosition.y += trajectory.y * delta_t;
-        delete[] scalars;
-
-        sprite.setPosition(newPosition);
-        position = newPosition;
-
+    if(newPosition.x < 0){
+        newPosition.x += windowX;
     }
+    if(newPosition.y < 0){
+        newPosition.y += windowY;
+    }
+    if(newPosition.x > windowX){
+        newPosition.x -= windowX;
+    }
+    if(newPosition.y > windowY){
+        newPosition.y -= windowY;
+    }
+
+    sprite.setPosition(newPosition);
+    position = newPosition;
+
     velocity.x *= friction;
     velocity.y *= friction;
 }
