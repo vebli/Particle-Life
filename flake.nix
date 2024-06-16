792b9f3e85a960aeefd597c6e2e48bd66bf3c25f
vebly <jh.haruna@gmail.com>
2024-06-16 22:47:57 +0200

    multip system support

--- flake.nix
+++ flake.nix
@@ -1,41 +1,51 @@
 {
-    inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
-    outputs = {self, nixpkgs, ...}: let 
-        system = "x86_64-linux";
-        pkgs = import nixpkgs {inherit system;};
-    in
-    {
-        packages.${system}.default = pkgs.stdenv.mkDerivation {
-                name = "Particle-Life";
-                src = ./.;
-                buildInputs = with pkgs; [
-                    gcc
-                    gnumake
-                    cmake
-                    sfml
-                    libGLU
-                    glfw-wayland
-                    glfw
-                ];
-                configurePhase = ''
-                    mkdir -p build   
-                    cd build  
-                    cmake -DCMAKE_BUILD_TYPE=Release ..
-                    mkdir -p bin
-                    '';
-                buildPhase = '' 
-                    cmake --build .
-                    '';
-                installPhase = ''
-                    mkdir -p "$out/bin"
-                    cp Particle-Life $out/bin/
-                '';
-        };
-
-        apps.${system}.default = {
-            type = "app";
-            program = "${self.packages.${system}.default}/bin/Particle-Life";
-        };
-
-    };
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    nix-systems.url = "github:nix-systems/default";
+  };
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    ...
+  }: let
+    eachSystem = nixpkgs.lib.genAttrs (import inputs.nix-systems);
+    mkPkgs = system: nixpkgs.legacyPackages.${system};
+  in {
+    packages = eachSystem (system: let
+      pkgs = mkPkgs system;
+    in {
+      default = pkgs.stdenv.mkDerivation {
+        name = "Particle-Life";
+        src = ./.;
+        buildInputs = with pkgs; [
+          gcc
+          gnumake
+          cmake
+          sfml
+          libGLU
+          glfw-wayland
+          glfw
+        ];
+        configurePhase = ''
+          mkdir -p build
+          cd build
+          cmake -DCMAKE_BUILD_TYPE=Release ..
+          mkdir -p bin
+        '';
+        buildPhase = ''
+          cmake --build .
+        '';
+        installPhase = ''
+          mkdir -p "$out/bin"
+          cp Particle-Life $out/bin/
+        '';
+      };
+    });
+    apps = eachSystem (system: {
+      default = {
+        type = "app";
+        program = "${self.packages.${system}.default}/bin/Particle-Life";
+      };
+    });
+  };
 }
